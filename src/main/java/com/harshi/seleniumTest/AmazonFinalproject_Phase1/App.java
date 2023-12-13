eef977d6de2fa028f4b1385290faf8bf7db1c3b5
harshithanap <ashwathharshitha0@gmail.com>
2023-12-13 16:04:39 +0530

    project setup for Amazon

---
+++ src/main/java/com/harshi/seleniumTest/AmazonFinalproject_Phase1/App.java
@@ -0,0 +1,13 @@
+package com.harshi.seleniumTest.AmazonFinalproject_Phase1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
